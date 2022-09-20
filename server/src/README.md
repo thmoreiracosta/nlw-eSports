ca922176029abf188997b72779a06a0d959f5073
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-09-20 14:08:20 -0300

    inicio da criação database

---
+++ server/src/README.md
@@ -0,0 +1,20 @@
+# Back-end
+
+## Entidades
+
+### Games
+
+id 
+title
+bannerUrl
+
+CDN (Content Delivery Network) => 
+
+### Ad
+
+is
+gameId
+name
+yearsPlaying
+discord
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,36 @@
-import express from 'express';
-
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json([
-    { id: 1, name: 'Anúncio 1' },
-    { id: 2, name: 'Anúncio 2' },
-    { id: 3, name: 'Anúncio 3' },    
-    { id: 4, name: 'Anúncio 4' },    
-  ])
+app.get("/games", (req, res) => {
+  return res.json([]);
 });
 
-app.listen(3000, () => {  
-  console.log('Server started on port 3000');
+app.post("/ads", (req, res) => {
+  return res.status(501).json([]);
 });
 
+app.get("/games/:id/ads", (req, res) => {
+
+  //const gameID = req.params.id;
+  //return res.send(gameID);
+  
+
+  return res.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+    { id: 4, name: "Anúncio 4" },
+  ]);
+});
+
+
+app.get("/ads/:id/discord", (req, res) => {  
+  const adId = req.params.id;
+
+  return res.json(adId);
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
