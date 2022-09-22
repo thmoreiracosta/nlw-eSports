0f180d55dd2911e3f6a1bb242438b573866f4187
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-09-22 15:52:27 -0300

    Consumindo API DB mobile

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
 import logo from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
-import { GAMES } from '../../utils/games';
-
 import { styles } from './styles';
 
+
 export function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.1.11:3000/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Image
@@ -19,9 +29,9 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <GameCard
             data={item}
           />
